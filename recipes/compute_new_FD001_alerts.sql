68b73ee5099f126159db926c2ae8c9fd0cb5d729
nanae <nanae.matsushima@dataiku.com>
2021-07-21 01:53:25 +0000

    Saved recipe: NANAE_SENSORANDFAILURES.compute_new_FD001_alerts

--- recipes/compute_new_FD001_alerts.sql
+++ recipes/compute_new_FD001_alerts.sql
@@ -1,2 +1,11 @@
-SELECT *
-  FROM "sandbox"."NANAE_SENSORANDFAILURES_new_fd001_scored"
+SELECT "T"."unit_id", "T"."Nb_Cycles", "T"."prediction", "T"."proba_long", "T"."proba_medium", "T"."proba_short", "T"."proba_urgent"
+FROM "${projectKey}_new_fd001_scored" "T",
+(
+SELECT 
+"unit_id" , max("Nb_Cycles") as "max_cycle"
+FROM "${projectKey}_new_fd001_scored"
+WHERE "prediction" in ('urgent','short')
+GROUP BY "unit_id"
+) R
+WHERE "T"."unit_id" = R."unit_id"
+AND "T"."Nb_Cycles" = R."max_cycle" 
