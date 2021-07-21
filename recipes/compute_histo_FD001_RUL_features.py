868ccc1a88a97e454cf2cf925f26007848c85b3d
nanae <nanae.matsushima@dataiku.com>
2021-07-21 01:24:30 +0000

    Saved recipe: NANAE_SENSORANDFAILURES.compute_histo_FD001_RUL_features

--- recipes/compute_histo_FD001_RUL_features.py
+++ recipes/compute_histo_FD001_RUL_features.py
@@ -1,8 +1,8 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
-
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Declare input and output objects
