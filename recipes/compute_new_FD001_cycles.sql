4fa1e2e69ad96fe04fd7e0253878b267f2e1425e
nanae <nanae.matsushima@dataiku.com>
2021-07-21 01:42:18 +0000

    Saved recipe: NANAE_SENSORANDFAILURES.compute_new_FD001_cycles

--- recipes/compute_new_FD001_cycles.sql
+++ recipes/compute_new_FD001_cycles.sql
@@ -1,2 +1,15 @@
-SELECT *
-  FROM "sandbox"."NANAE_SENSORANDFAILURES_new_fd001_prepared"
+SELECT "FD001".* , 
+
+(DATE_PART('day',"time" - "DateRef")*24*60
+    + DATE_PART('hour',"time" - "DateRef")*60
+    + DATE_PART('minute',"time" - "DateRef"))/30 as "Nb_Cycles"
+
+    
+FROM "${projectKey}_new_fd001_prepared" "FD001", 
+(
+SELECT "unit_id" , 
+       TO_TIMESTAMP('15/09/2018 10:00:00', 'DD/MM/YYYY HH24:MI:SS') as "DateRef"
+  FROM "${projectKey}_new_fd001_prepared"
+  GROUP BY "unit_id"
+    ) "R"
+WHERE "FD001"."unit_id" = "R"."unit_id"
