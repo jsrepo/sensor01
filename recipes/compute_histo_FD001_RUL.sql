42b8d8410639ace9cdf89d72534d2c0b454fb307
judy <judy>
2021-07-27 03:54:46 +0000

    Saved recipe: NANAE_SENSORANDFAILURES.compute_histo_FD001_RUL

--- recipes/compute_histo_FD001_RUL.sql
+++ recipes/compute_histo_FD001_RUL.sql
@@ -13,7 +13,7 @@ FROM "sandbox"."NANAE_SENSORANDFAILURES_histo_fd001" "FD001",
 (
 SELECT "unit_id" , max("time") as "failure_time" , 
        TO_TIMESTAMP('07/09/2018 10:00:00', 'DD/MM/YYYY HH24:MI:SS') as "DateRef"
-  FROM "${projectKey}_histo_fd001"
+  FROM "sandbox"."NANAE_SENSORANDFAILURES_histo_fd001"
   GROUP BY "unit_id"
     ) "R"
 WHERE "FD001"."unit_id" = "R"."unit_id"
