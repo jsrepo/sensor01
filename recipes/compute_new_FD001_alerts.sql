aaba2d4f64e19c6d6978eba1673e36eb933f2613
judy <judy>
2021-07-27 03:53:57 +0000

    Saved recipe: NANAE_SENSORANDFAILURES.compute_new_FD001_alerts

--- recipes/compute_new_FD001_alerts.sql
+++ recipes/compute_new_FD001_alerts.sql
@@ -1,9 +1,9 @@
 SELECT "T"."unit_id", "T"."Nb_Cycles", "T"."prediction", "T"."proba_long", "T"."proba_medium", "T"."proba_short", "T"."proba_urgent"
-FROM "${projectKey}_new_fd001_scored" "T",
+FROM "sandbox"."NANAE_SENSORANDFAILURES_new_fd001_scored" "T",
 (
 SELECT 
 "unit_id" , max("Nb_Cycles") as "max_cycle"
-FROM "${projectKey}_new_fd001_scored"
+FROM "sandbox"."NANAE_SENSORANDFAILURES_new_fd001_scored"
 WHERE "prediction" in ('urgent','short')
 GROUP BY "unit_id"
 ) R
