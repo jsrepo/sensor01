d76c6336903bd67a005f697c1856c83742714778
nanae <nanae.matsushima@dataiku.com>
2021-07-28 07:16:27 +0000

    Recipe NANAE_SENSORANDFAILURES.recipe_from_notebook_Sensor_and_Failure saved through notebook Sensor_and_Failure_Demo

--- recipes/recipe_from_notebook_Sensor_and_Failure.py
+++ recipes/recipe_from_notebook_Sensor_and_Failure.py
@@ -6,8 +6,8 @@ from dataiku import pandasutils as pdu
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Declare input and output objects
-dku_input = dataiku.Dataset("histo_FD001_RUL_prepared_movingavg")
-dku_output = dataiku.Dataset("histo_FD001_RUL_features")
+dku_input = dataiku.Dataset("engine_hiso")
+dku_output = dataiku.Dataset("engine_histo_prepared")
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 df_input = dku_input.get_dataframe()
@@ -56,4 +56,3 @@ for i in range(1,22):
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 dku_output.write_with_schema(df_output)
-
